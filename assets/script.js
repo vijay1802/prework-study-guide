e7bf5c775aea33144ca6d9a9f749e764f359b359
Vijay <v9roopan@gmail.com>
2023-10-15 09:46:10 +0100

    java script added to the project

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+// var topic = 'HTML';
+
+// if (topic === 'HTML') {
+//   console.log("Let's Study HTML!");
+// } else if (topic === 'CSS') {
+//   console.log("Let's Study CSS!");
+// } else if (topic === 'Git') {
+//   console.log("Let' s Study Git!");
+// } else if (topic === 'JavaScript') {
+//   console.log("Let' s Study JavaScript!");
+// } else {
+//   console.log('Please try again');
+// }
+
+var shapes = ['triangle', 'square', 'pentagon', 'circle'];
+// console.log(shapes[0]);
+// console.log(shapes[1]);
+// console.log(shapes[2]);
+// console.log(shapes[3]);
+
+for (var x = 0; x < shapes.length; x++) {
+  console.log(shapes[x]);
+}
+
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+for (var x = 0; x < topics.length; x++) {
+  console.log(topics[x]);
+}
